f6f918c723341c8bb3ff9aa0a88e33617b388246
filosfino <github@filosfino.com>
2020-04-25 21:17:51 +0800

    feat(299,441,589,590): 299,441,589,590

---
+++ Algorithms/Q590_n叉树的后序遍历.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode.cn id=590 lang=python3
+#
+# [590] N叉树的后序遍历
+#
+# https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal/description/
+#
+# algorithms
+# Easy (72.93%)
+# Likes:    61
+# Dislikes: 0
+# Total Accepted:    20.8K
+# Total Submissions: 28.5K
+# Testcase Example:  '[1,null,3,2,4,null,5,6]'
+#
+# 给定一个 N 叉树，返回其节点值的后序遍历。
+#
+# 例如，给定一个 3叉树 :
+#
+#
+#
+#
+#
+#
+#
+# 返回其后序遍历: [5,6,3,2,4,1].
+#
+#
+#
+# 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        ret = []
+        if not root:
+            return ret
+        q = [root]
+        while q:
+            i = q.pop()
+            ret.append(i.val)
+            q.extend(i.children)
+        return ret[::-1]
+
+# @lc code=end
+
